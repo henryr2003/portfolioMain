5519feaa09a7e4646c0916d9466431422705af6a
henryr2003 <henryrodas2003@gmail.com>
2025-10-14 23:31:49 -0700

    fixed mobile view shift when animation occurs

--- src/app/Projects.js
+++ src/app/Projects.js
@@ -5,26 +5,69 @@ import {motion} from "framer-motion";
 import portfolioPic from "../img/portfolioPic.png";
 import vet from "../img/vet.png";
 
+import { useMediaQuery } from "./hooks/useMediaQuery";
+
 export default function Projects({projectRef}) {
 
-
-    
-
+    const isMobile = useMediaQuery("(max-width: 768px)");
 
     return (
 
 
         <>
 
-            <main ref={projectRef} className="w-full h-auto flex flex-col gap-[32px] row-start-2 items-center sm:items-start bg-gray-800 font-mono">
+            <main ref={projectRef} className="w-full h-auto relative overflow-hidden flex flex-col gap-[32px] row-start-2 items-center sm:items-start bg-gray-800 font-mono">
   
             
 
-                <motion.div className="w-full h-[700px] flex pl-20 gap-30 mt-50"
+                <motion.div className="w-full h-auto gap-10 flex flex-col justify-center items-center  mt-50 md:flex-row md:pl-20 md:gap-30 md:h-[700px]"
                 
                 initial={{
                     opacity: 0,
-                    x: "-500px",
+                    x: "-100px",
+                }}
+                
+                whileInView={{
+
+                    opacity: 1,
+                    x: 0,
+                }}
+
+                transition={{
+                    default: {duration: 1.5, ease: "easeInOut"}
+                }}
+
+                
+
+                viewport={{
+                    amount: 0.3,
+                    once: true,
+                }}
+
+
+
+                >   
+
+                    {isMobile && <h1 className="text-md text-white md:text-4xl"> Personal Portfolio Website </h1> }
+
+                    <Image alt="Image of this portfolio website" src={portfolioPic} className="w-[90%] h-auto border-2 border-white md:w-auto md:h-[600px] "/>
+
+                
+
+                    <div className=" w-[90%] text-center h-auto mb-20 text-white md:w-[600px] md:h-[600px]">
+
+                        {!isMobile && <h1 className="text-md mb-20 md:text-4xl"> Personal Portfolio Website </h1> }
+                        <h2 className="text-sm leading-loose md:text-2xl"> My first time using Next.js and Framer Motion for smooth animations. Learned lots about motion and UI/UX and will use what I learned in future projects.</h2>
+                    </div>
+
+                </motion.div>
+
+
+                <motion.div className="w-full h-auto flex mb-20 justify-center items-center text-center flex-col md:flex-row md:gap-30 md:pr-20 md:h-[700px] "
+                
+                initial={{
+                    opacity: 0,
+                    x: "100px",
                 }}
 
                 whileInView={{
@@ -43,58 +86,19 @@ export default function Projects({projectRef}) {
                     amount: 0.2,
                     once: true,
                 }}
-
-
-
                 >
 
-                    <Image alt="Image of this portfolio website" src={portfolioPic} className="w-auto h-[600px] border-2 border-white"/>
+                    <div className="w-[90%] h-auto  text-white md:w-[600px] ">
 
-                
-
-                    <div className="w-[600px] h-[600px] text-white  ">
-
-                        <h1 className="text-4xl mb-20"> Personal Portfolio Website </h1>
-                        <h2 className="text-2xl leading-loose "> My first time using Next.js and Framer Motion for smooth animations. Learned lots about motion and UI/UX and will use what I learned in future projects.</h2>
-                    </div>
-
-                </motion.div>
-
-
-                <motion.div className="w-full h-[700px] flex justify-end pr-20 gap-30 mt-50"
-                
-                initial={{
-                    opacity: 0,
-                    x: "500px",
-                }}
-
-                whileInView={{
-
-                    opacity: 1,
-                    x: 0,
-                }}
-
-                transition={{
-                    default: {duration: 1.5, ease: "easeInOut"}
-                }}
-
-                
-
-                viewport={{
-                    amount: 0.3,
-                    once: true,
-                }}
-                >
-
-                    <div className="w-[600px] h-[600px] text-white  ">
-
-                        <h1 className="text-4xl mb-20"> Veterinary Clinic Waitlist </h1>
-                        <h2 className="text-2xl leading-loose "> Waitlist for customers who are waiting for their pets' surgery. Gives them updates on their pet's progress and lets them know estimated finish times. Also
+                        <h1 className="text-md  mb-20 md:text-4xl"> Veterinary Clinic Waitlist </h1>
+                        <h2 className="text-md  leading-loose md:text-2xl"> Waitlist for customers who are waiting for their pets' surgery. Gives them updates on their pet's progress and lets them know estimated finish times. Also
                         has login functionality for staff so that they can add and edit information about customers' pets. 
                         </h2>
+                        
                     </div>
 
-                    <Image alt="Image of veterinary website" src={vet}className="w-auto h-[600px] border-2 border-white"/>
+                    <Image alt="Image of veterinary website" src={vet}className="w-[90%] h-auto border-2 border-white md:w-auto md:h-[600px]"/>
+                    
 
                     
 
